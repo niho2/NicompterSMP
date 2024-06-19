5e42af2b1999862eee14bd6e7a4dfc12f6528925
Nico <nico@nicompter.de>
2024-06-19 22:12:48 +0200

    Added Creeper Reaction Recording via Advanced Replay Plugin

--- src/main/java/de/cool_miner2021/nicomptersmp/commands/Creeper.java
+++ src/main/java/de/cool_miner2021/nicomptersmp/commands/Creeper.java
@@ -2,12 +2,16 @@ package de.cool_miner2021.nicomptersmp.commands;
 
 import de.cool_miner2021.nicomptersmp.NicompterSMP;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Creeper implements CommandExecutor {
     @Override
@@ -21,7 +25,24 @@ public class Creeper implements CommandExecutor {
             commandSender.sendMessage(NicompterSMP.prefix+"§cDieser Spieler konnte nicht gefunden werden");
             return false;
         }
-        target.playSound(target.getLocation(), Sound.ENTITY_CREEPER_PRIMED,3.0f,1.0f);
+
+        // Generate a unique replay name based on the current time and player's name
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+        String replayName = "CR_" + target.getName() + "_" + timestamp;
+
+        // Start recording 1 second before playing the sound
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "replay start " + replayName + ":10 " + target.getName());
+
+        // Schedule the sound to be played after 1 second (20 ticks)
+        Bukkit.getScheduler().runTaskLater(NicompterSMP.getPlugin(NicompterSMP.class), () -> {
+            Location location = target.getLocation();
+            target.playSound(location, Sound.ENTITY_CREEPER_PRIMED, 3.0f, 1.0f);
+
+        }, 20L); // 1 second delay before playing the sound
+
+
+
+        // target.playSound(target.getLocation(), Sound.ENTITY_CREEPER_PRIMED,3.0f,1.0f);
         commandSender.sendMessage(NicompterSMP.prefix+"§aCreeper Sound wurde erfolgreich bei §2"+target.getName()+"§a abgespielt");
         return true;
     }
